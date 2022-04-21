79d6eeb9b14d5b51577eaa57c8918ba69d518332
andrea_bianchi24 <andrea.bianchi24@studio.unibo.it>
2022-04-21 15:26:24 +0200

    created zipRight (which uses a makeTuple method inside) and partition methods in List.scala. Also first draft for a possible span method solution

--- src/main/scala/u05lab/ex1/List.scala
+++ src/main/scala/u05lab/ex1/List.scala
@@ -59,16 +59,26 @@ enum List[A]:
   def reverse(): List[A] = foldLeft[List[A]](Nil())((l, e) => e :: l)
 
   /** EXERCISES */
-  def zipRight: List[(A, Int)] = ???
+  def zipRight: List[(A, Int)] =
+    def makeTuples(elements: List[A], n: Int): List[(A,Int)] = elements match
+      case h :: t => (h, n) :: makeTuples(t, n + 1)
+      case _ => Nil()
+    makeTuples(this, 0)
 
-  def partition(pred: A => Boolean): (List[A], List[A]) = ???
+  def partition(pred: A => Boolean): (List[A], List[A]) =
+    (this.filter(pred), this.filter(el => !pred(el)))
 
-  def span(pred: A => Boolean): (List[A], List[A]) = ???
+  def span(pred: A => Boolean): (List[A], List[A]) =
+    def getFirstList(el: List[A]): List[A] = el match
+      case h :: t if pred(h) => h :: getFirstList(t)
+      case _ => Nil()
+    (getFirstList(this), )
+
 
   /** @throws UnsupportedOperationException if the list is empty */
   def reduce(op: (A, A) => A): A = ???
 
-  def takeRight(n: Int): List[A] = ???
+  def takeRight(n: Int): List[A] = ???*/
 
 // Factories
 object List:
@@ -84,11 +94,14 @@ object List:
 @main def checkBehaviour(): Unit =
   val reference = List(1, 2, 3, 4)
   println(reference.zipRight) // List((1, 0), (2, 1), (3, 2), (4, 3))
+
   println(reference.partition(_ % 2 == 0)) // (List(2, 4), List(1, 3))
+
   println(reference.span(_ % 2 != 0)) // (List(1), List(2, 3, 4))
+  /*
   println(reference.span(_ < 3)) // (List(1, 2), List(3, 4))
   println(reference.reduce(_ + _)) // 10
   try Nil.reduce[Int](_ + _)
   catch case ex: Exception => println(ex) // prints exception
   println(List(10).reduce(_ + _)) // 10
-  println(reference.takeRight(3)) // List(2, 3, 4)
+  println(reference.takeRight(3)) // List(2, 3, 4)*/
