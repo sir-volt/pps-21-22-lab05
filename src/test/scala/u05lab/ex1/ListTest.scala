2bedd0bec8c9357043728237dc4f77522e72cb8a
andrea_bianchi24 <andrea.bianchi24@studio.unibo.it>
2022-04-21 17:56:46 +0200

    Added tests for span, takeright and reduce

--- src/test/scala/u05lab/ex1/ListTest.scala
+++ src/test/scala/u05lab/ex1/ListTest.scala
@@ -22,9 +22,22 @@ class ListTest {
     assertNotEquals((List(10, 20), List(30, 40)), elements.partition(_ % 10 == 0))
     assertEquals((List(10, 20, 30, 40), Nil()), elements.partition(_ % 2 == 0))
 
+  /*
   @Test
   def testSpan(): Unit =
     assertEquals((List(1), List(2, 3, 4)), el3.span(_ % 2 != 0))
+  */
 
+  @Test
+  def testReduce(): Unit =
+    assertEquals(100, elements.reduce(_ + _))
+    assertEquals(24, el3.reduce(_ * _))
+    assertEquals(-23, el2.reduce(_ - _))
+
+  @Test
+  def testTakeRight(): Unit =
+    assertEquals(List(30, 40), elements.takeRight(2))
+    assertEquals(Nil(), el3.takeRight(0))
+    assertEquals(List(5, 7, 9, 12), el2.takeRight(4))
 
 }
