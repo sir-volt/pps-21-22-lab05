b70763b2965f896a19696adcb8e2af6d0c04fadd
andrea_bianchi24 <andrea.bianchi24@studio.unibo.it>
2022-04-28 10:27:21 +0200

    added tests for each required method (orederedScores, average, acceptedArticles...) moved file into test/scala folder in a new folder in order to use junit

--- src/test/scala/u05lab/ex2/ConferenceTest.scala
+++ src/test/scala/u05lab/ex2/ConferenceTest.scala
@@ -1,27 +1,52 @@
 package u05lab.ex2
 
-import org.junit.Test
-import org.junit.Before
 import org.junit.Assert.*
+import org.junit.Test
+import u05lab.ex2.{ConferenceReviewing, Question}
 
 import scala.collection.immutable.Map
-import u05lab.ex2.Question
-import u05lab.ex2.ConferenceReviewing
 
-class ConferenceTest
+class ConferenceTest {
 
   var randomMap: Map[Question, Int] = Map(Question.RELEVANCE -> 8, Question.SIGNIFICANCE -> 8,
-  Question.CONFIDENCE -> 7, Question.FINAL -> 8)
+    Question.CONFIDENCE -> 7, Question.FINAL -> 8)
   var conf = ConferenceReviewing
 
   conf.loadReview(1, 8, 8, 6, 8)
   conf.loadReview(1, 9, 9, 6, 9)
   conf.loadReview(2, 4, 6, 10, 6)
+  conf.loadReview(2, 9, 9, 10, 9)
   conf.loadReview(3, 4, 4, 4, 4)
+  conf.loadReview(3, 3, 3, 3, 3)
+  conf.loadReview(4, 7, 7, 8, 7)
+  conf.loadReview(4, 6, 6, 6, 6)
   conf.loadReview(4, randomMap)
+  conf.loadReview(5, 6, 6, 6, 10)
+  conf.loadReview(5, 7, 7, 7, 10)
 
+  @Test
+  def testOrderedScores(): Unit =
+    assertEquals(conf.orderedScores(2, Question.RELEVANCE), List(4, 9))
+    assertEquals(conf.orderedScores(4, Question.CONFIDENCE), List(6, 7, 8))
+    assertEquals(conf.orderedScores(1, Question.FINAL), List(8, 9))
 
+  @Test
+  def testAverageFinalScore(): Unit =
+    assertEquals(conf.averageFinalScore(1), 8.5, 0.01)
+    assertEquals(conf.averageFinalScore(2), 7.5, 0.01)
+    assertEquals(conf.averageFinalScore(3),3.5,0.01)
+    assertEquals(conf.averageFinalScore(4),7.0,0.01)
+    assertEquals(conf.averageFinalScore(5),10.0,0.01)
 
+  @Test
+  def testAcceptedArticles(): Unit =
+    assertEquals(conf.acceptedArticles(), List(1,2,4).toSet)
+
+  
+  @Test
+  def testSortedAcceptedArticles(): Unit =
+    assertEquals(conf.sortedAcceptedArticles(),List((4,7.0), (2,7.5), (1,8.5)))
+}
 /*
 
 
